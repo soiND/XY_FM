3aaf14392d6dded687cc6e98540002704160ddfd
许郁 <yuxua@isoftstone.com>
2017-05-22 14:44:45 +0800

    测试版本哈哈3

---
+++ XuYuFM/Classes/TestVC3.h
@@ -0,0 +1,13 @@
+//
+//  TestVC3.h
+//  XuYuFM
+//
+//  Created by 许郁 on 2017/5/22.
+//  Copyright © 2017年 xxyy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TestVC3 : UIViewController
+
+@end
---
+++ XuYuFM/Classes/TestVC3.m
@@ -0,0 +1,37 @@
+//
+//  TestVC3.m
+//  XuYuFM
+//
+//  Created by 许郁 on 2017/5/22.
+//  Copyright © 2017年 xxyy. All rights reserved.
+//
+
+#import "TestVC3.h"
+
+@interface TestVC3 ()
+
+@end
+
+@implementation TestVC3
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
